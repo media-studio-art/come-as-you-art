7062841b98d48dc91b8c3cf69ba4dc0fef484269
media-studio-art <classesuniversity4@gmail.com>
2026-02-13 13:18:01 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const { createApp, ref } = Vue;
+const { createVuetify } = Vuetify;
+const vuetify = createVuetify();
+
+const App = {
+    setup() {
+        return {
+
+
+        }
+    }
+}
+
+createApp(App).use(vuetify).mount('#app');
